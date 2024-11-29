611263d9654046ccdbff93e48cd70787a80ad787
Deranindu Gunasekara <140329900+DeraniDU@users.noreply.github.com>
2024-11-29 09:46:07 +0530

    implement navigation bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react'
 export default class NavBar extends Component {
   render() {
     return (
-      <div>NavBar</div>
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <div class="container">
+            <a class="navbar-brand" href="/">Posts</a>
+         </div>
+        </nav>
+      </div>
     )
   }
 }
